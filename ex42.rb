6ffc0c72dcfe0bf12c1d09ab05bfea6f7ed112b2
Michu Lugs <peachyjello@gmail.com>
2014-01-25 20:41:23 -0500

    completed exercise 42

--- ex42.rb
+++ ex42.rb
@@ -83,3 +83,92 @@ class Game
 		guess = gets.chomp()
 		guesses = 0
 		
+		while guess != code and guesses < 10
+			puts "BZZZT!"
+			guesses += 1
+			print "[keypad]> "
+			guess = gets.chomp()
+		end
+
+		if guess == code
+			puts "The container clicks open and the seal breaks, letting gas out."
+			puts "You grab the neutron bomb and run as fast as you can to the"
+			puts "bridge where you must place it in the right spot."
+			return :the_bridge
+		else
+			puts "The lock buzzes one last time and then you hear a sickening"
+			puts "melting sound as the mechanism is fused together."
+			puts "You decide to sit there, and finally the Gothons blow up"
+			puts "the ship from their ship, and you die."
+			return :death
+		end
+	end
+
+	def the_bridge()
+		puts "You burst onto the Bridge with the neutron destruct bomb"
+		puts "under your arm and surprise 5 Gothons who are trying to"
+		puts "take control of the ship. Each of tehm has an even uglier"
+		puts "clown costume than the last. They haven't pulled their"
+		puts "weapons out yet, as they see the active bomb under your"
+		puts "arm and don't want to set it off."
+
+		prompt()
+		action = gets.chomp()
+
+		if action == "throw the bomb"
+			puts "In a panic you throw the bomb at the group of Gothons"
+			puts "and make a leap for the door. Right as you drop it a"
+			puts "Gothon shoots your irhgt in the back, killing you."
+			puts "As you die you see another Gothon frantically trying to disarm"
+			puts "the bomb. You die knowing they will probably blow up when"
+			puts "it goes off."
+			return :death
+		elsif action == "slowly place the bomb"
+			puts "You point your blaster at the bomb under your arm"
+			puts "and the Gothons put their hands up and start to sweat."
+			puts "You inch backward tot he door, open it, and then carefully"
+			puts "place the bomb on the floor, pointing your blaster at it."
+			puts "You then jump back through the door, punch the close button"
+			puts "and blast the lock so that the Gothons can't get out."
+			puts "Now that the bomb is placed you run to the escape pod to"
+			puts "get off this tin can."
+			return :escape_pod
+		else
+			puts "DOES NOT COMPUTE!"
+			return :the_bridge
+		end
+	end
+
+	def escape_pod()
+		puts "You rush through the ship desperately trying to make it to"
+		puts "the escape pod before the whole ship explodes. It seems like"
+		puts "hardly any Gothons are on the ship, so your run is clear of"
+		puts "interference. You get tot he chamber with the escape pods, and"
+		puts "now need to pick one to take. Some of the could be damaged"
+		puts "but you don't have the time to look. There's 5 pods, which one"
+		puts "do you take?"
+
+		good_pod = rand(5)+1
+		print "[pod #]> "
+		guess = gets.chomp()
+
+		if guess.to_i != good_pod
+			puts "You jump into pod %s and hit the eject button." % guess
+			puts "The pod escapes out into the void of space, then"
+			puts "implodes as the hull ruptures, crushing your body"
+			puts "into jam jelly."
+			return :death
+		else
+			puts "You jump into pod %s and hit the eject button." % guess
+			puts "THe pod easily slides out into space heading to"
+			puts "the planet below. As it flies tot he planet, you look"
+			puts "back and see your ship implode then explode like a"
+			puts "bright star, taking out the Gothon ship at the same"
+			puts "time. You won!"
+			Process.exit(0)
+		end
+	end
+end
+
+a_game = Game.new(:central_corridor)
+a_game.play()
